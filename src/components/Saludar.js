64e9a2e67f3eda47b9996d22a536daf9ebb93e28
jorgedeltoro111 <ingjorgedeltoro@gmail.com>
2023-12-29 00:08:24 -0600

    Validation props

--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Text} from 'react-native';
-
+import PropTypes from 'prop-types';
 export default function Saludar(props){
     //pasando propiedades por defecto directamente en la destructuracion del objeto
     //const { name='Enrique' } = props;
@@ -12,4 +12,8 @@ export default function Saludar(props){
 //Mejor forma de usar propiedades de manera default
 Saludar.defaultProps = {
     name: 'Enrique',
+}
+//Requerimientos de tipos para las propiedades
+Saludar.propTypes = {
+    name: PropTypes.string.isRequired
 }
